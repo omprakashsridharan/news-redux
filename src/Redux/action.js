ba68a70ca237d2fb512190c063a5e9c47cfa241a
Omprakash <omprakashsridharan@outlook.com>
2018-05-11 22:42:45 +0530

    Modify

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -14,6 +14,7 @@ export function getArticles(){
     }
 }
 
+
 function callApi(){
     return (
         fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=58df8c1651ce4704aaa5432072bc0f02')
